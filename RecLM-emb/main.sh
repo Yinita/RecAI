a3dbc56ca8df8822ff682cb46e38759293b6f65a
v-yitiahuang <1656211644@qq.com>
2024-10-25 13:20:52 -0700

    .

--- RecLM-emb/main.sh
+++ RecLM-emb/main.sh
@@ -8,7 +8,7 @@ export OPENAI_API_BASE="http://localhost:8101/v1"
 export OPENAI_API_TYPE="onlinevllm"
 export OPENAI_API_VERSION="v1"
 export MODEL="Qwen/Qwen2.5-72B-Instruct"
-export batch_size=300
+export batch_size=500
 export model_altname="qwen72B"
 export model_altname_v2="qwen72B_v2"
 
@@ -22,41 +22,27 @@ export model_altname_v2="qwen72B_v2"
 export learning_rate=5e-5
 export num_train_epochs=5
 export QUERY_MAX_LEN=1024
-export version=qwen_v1
+export version="1026"
 # model = gpt-4o
 # model_altname = gpt4
-export OUTPUT_DIR=output/xbox/reclm_emb_xbox_bge-m3_$version
+export OUTPUT_DIR=output/xbox/bge-m3_$version
 export MODEL_NAME_OR_PATH="BAAI/bge-m3" # Currently support BAAI/bge-m3 (best)    intfloat/e5-large-v2, bert-large-uncased, BAAI/bge-large-en-v1.5, meta-llama/Llama-2-7b-hf
 export RUN_NAME="bge_m3_$version"
 export TASK="xbox"
-# bash shell/data_pipeline.sh
-# bash shell/test_data_pipeline.sh
-# bash shell/run_single_node.sh
-
-export OUT_DIR="output/xbox_infer/text-embedding-3-large"
-export MODEL_PATH_OR_NAME=text-embedding-3-large
+bash shell/data_pipeline.sh
+bash shell/test_data_pipeline.sh
+cp -r /home/aiscuser/RecAI/RecLM-emb/output /home/aiscuser/figllm/toolcall/database/localdb/backup_data/1026
+bash shell/run_single_node.sh
+export OUT_DIR="output/xbox_infer/$RUN_NAME"
+export MODEL_PATH_OR_NAME=$OUTPUT_DIR
 bash shell/infer_metrics.sh 
+cp -r /home/aiscuser/RecAI/RecLM-emb/output /home/aiscuser/figllm/toolcall/database/localdb/backup_data/1026
+# export OUT_DIR="output/xbox_infer/text-embedding-3-large"
+# export MODEL_PATH_OR_NAME=text-embedding-3-large
+# bash shell/infer_metrics.sh 
 
 
 
-# export learning_rate=3e-5
-# export num_train_epochs=5
-
-# export QUERY_MAX_LEN=1024
-# export version=2
-# # model = Gpt-4-Turbo
-# # model_altname = gpt4
-# export OUTPUT_DIR=output/xbox/reclm_emb_xbox_bge-m3_$version
-# export MODEL_NAME_OR_PATH="BAAI/bge-m3" # Currently support BAAI/bge-m3 (best)    intfloat/e5-large-v2, bert-large-uncased, BAAI/bge-large-en-v1.5, meta-llama/Llama-2-7b-hf
-# export RUN_NAME="bge_m3_$version"
-# export TASK="xbox"
-# # bash shell/data_pipeline.sh
-# # bash shell/test_data_pipeline.sh
-# # bash shell/run_single_node.sh
-
-# export OUT_DIR="output/xbox_infer/$RUN_NAME"
-# export MODEL_PATH_OR_NAME=$OUTPUT_DIR
-# # bash shell/infer_metrics.sh 
 
 
 
